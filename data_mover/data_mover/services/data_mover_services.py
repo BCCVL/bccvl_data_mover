e25b2f1bc1c6838c79ab9e1fa5a5b720732c1598
Daniel Yazbek <daniel@intersect.org.au>
2013-08-08 17:42:45 +1000

    BCCVL-124 Minor cleanup

--- data_mover/data_mover/services/data_mover_services.py
+++ data_mover/data_mover/services/data_mover_services.py
@@ -1,24 +1,33 @@
 from pyramid_xmlrpc import XMLRPCView
 from data_mover.models import *
-from data_mover.scripts.worker_queue import background_queue
 
 class DataMoverServices(XMLRPCView):
 
-	def move(self, destination, source):
-		#create a new job
-		job = Job(type = source['type'], data_id = source['id'], destination = destination['host'])
-		DBSession.add(job)
-		DBSession.flush()
-		
-		#get source details from dataset manager
+    def move(self, destination_args, source_args):
 
-		#put into job queue
-		
-		#create the response
-		response = {'id': job.id, 'status': job.status}
-		return response
+        type = source_args['type']
+        data_id = source_args['id']
+        destination = destination_args['host']
 
-	def check(self, id):
-		job = DBSession.query(Job).get(id)
-		response = {'id': job.id, 'status': job.status}
-		return response
+        # TODO: Perform validation on the inputs and return early on error
+
+        # TODO: Query the dataset manager to obtain the source details of the file requested - should probably be persisted along with the job
+        # TODO: return early if the file can't be found.
+
+        # create a new job
+        job = Job(type, data_id, destination)
+
+        DBSession.add(job)
+        DBSession.flush()
+
+
+        #put into job queue so it can be moved
+
+        #create the response
+        response = {'id': job.id, 'status': job.status}
+        return response
+
+    def check(self, id):
+        job = DBSession.query(Job).get(id)
+        response = {'id': job.id, 'status': job.status}
+        return response
