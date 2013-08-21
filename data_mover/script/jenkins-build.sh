f0d7e76ca52386dcb66b03525728cd59332762bd
Daniel Yazbek <daniel@intersect.org.au>
2013-08-21 17:23:10 +1000

    BCCVL-124 Add support to build from Jenkins

---
+++ data_mover/script/jenkins-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$WORKSPACE" ]; then
+    echo "Guessing WORKSPACE is .."
+    WORKSPACE='..'
+fi
+
+echo "Using WORKSPACE $WORKSPACE"
+cd $WORKSPACE
+
+echo "Setting up virtualenv in $WORKSPACE"
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
+tar -xvzf virtualenv-1.9.tar.gz
+cd virtualenv-1.9
+/usr/bin/env python26 virtualenv.py data_mover
+cd data_mover
+source bin/activate
+
+
+
