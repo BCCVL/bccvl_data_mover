4ccbbb595a5aef7ce77509a74449caaea0103267
Daniel Yazbek <daniel@intersect.org.au>
2013-08-21 18:08:22 +1000

    BCCVL-124 Add support to build from Jenkins

--- data_mover/script/jenkins-build.sh
+++ data_mover/script/jenkins-build.sh
@@ -5,19 +5,20 @@ if [ -z "$WORKSPACE" ]; then
     WORKSPACE='..'
 fi
 
-WORKSPACE_PYTHON="$WORKSPACE/data_mover/bin/python26"
+#WORKSPACE_PYTHON="$WORKSPACE/data_mover/bin/python26"
 WORKING_DIR="$WORKSPACE/data_mover"
 
 echo "Using WORKSPACE $WORKSPACE"
 cd "$WORKSPACE"
 
-echo "Setting up virtualenv in $WORKSPACE"
-curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
-tar -xvzf virtualenv-1.9.tar.gz
-cd virtualenv-1.9
-/usr/bin/env python26 virtualenv.py --no-setuptools "$WORKING_DIR"
-cd "$WORKING_DIR"
-source bin/activate
+#echo "Setting up virtualenv in $WORKSPACE"
+#curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.tar.gz
+#tar -xvzf virtualenv-1.9.tar.gz
+#cd virtualenv-1.9
+#/usr/bin/env python26 virtualenv.py --no-setuptools "$WORKING_DIR"
+#cd "$WORKING_DIR"
+#source bin/activate
 
-#"$WORKSPACE_PYTHON" setup.py develop 
-"$WORKSPACE_PYTHON" bootstrap.py
+#"$WORKSPACE_PYTHON" setup.py develop
+
+python bootstrap.py
