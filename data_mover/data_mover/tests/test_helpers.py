cd9411041c2b57d3b50e90ee2e70c16521918c93
khanh2907 <khanh@intersect.org.au>
2013-08-21 17:50:35 +1000

    BCCVL-124: Add unit tests for scripts and helpers

--- data_mover/data_mover/tests/test_helpers.py
+++ data_mover/data_mover/tests/test_helpers.py
@@ -2,9 +2,7 @@ import unittest
 import transaction
 
 from pyramid import testing
-
 from sqlalchemy import create_engine
-
 from data_mover.models import *
 
 class TestHelpers(unittest.TestCase):
@@ -22,3 +20,40 @@ class TestHelpers(unittest.TestCase):
         DBSession.configure(bind=engine)
         # Drop all the models
         Base.metadata.drop_all(engine)
+
+### Test data_mover ###
+    # Test scp_to() success
+    def testDataMoverSCPTo(self):
+        from data_mover.helpers.data_mover import scp_to
+        host = Host('BCCVL_HPC', '115.146.85.28', 1, 'ubuntu', 'password')
+        destination_path = "sample/sample_destination_test/3"
+        source_path = "sample/sample_local/3"
+        response = scp_to(host, source_path, destination_path)
+        self.assertTrue(response)
+
+    # Test scp_to() fail
+    def testDataMoverSCPToFail(self):
+        from data_mover.helpers.data_mover import scp_to
+        host = None
+        destination_path = "sample/sample_destination_test/3"
+        source_path = "sample/sample_local/3"
+        response = scp_to(host, source_path, destination_path)
+        self.assertFalse(response)
+
+    # Test scp_from success
+    def testDataMoverSCPFrom(self):
+        from data_mover.helpers.data_mover import scp_from
+        host = Host('BCCVL_HPC', '115.146.85.28', 1, 'ubuntu', 'password')
+        destination_path = "sample/sample_local_test/3"
+        source_path = "sample/sample_source/3"
+        response = scp_from(host, source_path, destination_path)
+        self.assertTrue(response)
+
+    # Test scp_from fail
+    def testDataMoverSCPFromFail(self):
+        from data_mover.helpers.data_mover import scp_from
+        host = None
+        destination_path = "sample/sample_local_test/3"
+        source_path = "sample/sample_source/3"
+        response = scp_from(host, source_path, destination_path)
+        self.assertFalse(response)
