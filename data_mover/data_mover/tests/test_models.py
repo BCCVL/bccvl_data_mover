7cb6012b8fe7f189ad42e12516d44004f59a099d
khanh2907 <khanh@intersect.org.au>
2013-08-23 10:09:32 +1000

    BCCVL-124: Change test setup to support Jenkins

--- data_mover/data_mover/tests/test_models.py
+++ data_mover/data_mover/tests/test_models.py
@@ -11,14 +11,14 @@ class TestModels(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
-        engine = create_engine('postgresql+psycopg2://data_mover:data_mover@localhost:5432/data_mover_test')
+        engine = create_engine('sqlite:///:tmp:')
         DBSession.configure(bind=engine)
         Base.metadata.create_all(engine)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
-        engine = create_engine('postgresql+psycopg2://data_mover:data_mover@localhost:5432/data_mover_test')
+        engine = create_engine('sqlite:///:tmp:')
         DBSession.configure(bind=engine)
         # Drop all the models
         Base.metadata.drop_all(engine)
