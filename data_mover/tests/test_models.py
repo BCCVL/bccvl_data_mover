8404815d60dbfd1649ac73e51442d3a0d83d0e9f
khanh2907 <khanh@intersect.org.au>
2013-09-03 15:25:32 +1000

    BCCVL-124: Remove host and protocol models tests

--- data_mover/tests/test_models.py
+++ data_mover/tests/test_models.py
@@ -1,8 +1,5 @@
 import unittest
 from data_mover.models.job import Job
-from data_mover.models.protocol import Protocol
-from data_mover.models.host import Host
-
 
 class TestModels(unittest.TestCase):
 
@@ -18,25 +15,3 @@ class TestModels(unittest.TestCase):
         self.assertIsNone(job.start_timestamp)
         self.assertIsNone(job.end_timestamp)
         self.assertEqual('sample/sample_source', job.source)
-
-    def testProtocolModel(self):
-        name = 'RCP'
-        command = 'rcp'
-        options = '-x -t -z'
-        protocol = Protocol(name, command, options)
-        self.assertEqual(name, protocol.name)
-        self.assertEqual(command, protocol.command)
-        self.assertEqual(options, protocol.options)
-
-    def testHostModel(self):
-        name = 'someHost'
-        server = '192.168.0.1'
-        protocol = Protocol('SCP', 'scp', '')
-        user = 'admin'
-        password = 'myPass'
-        host = Host(name, server, protocol, user, password)
-        self.assertEqual(name, host.name)
-        self.assertEquals(server, host.server)
-        self.assertEquals(protocol, host.protocol)
-        self.assertEquals(user, host.user)
-        self.assertEquals(password, host.password)
