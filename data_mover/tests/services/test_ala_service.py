a170bcba61176ca2e404d20ec02b2c64bf555edc
Khanh Nguyen <khanh@intersect.org.au>
2014-01-16 09:51:42 +1100

    BCCVL-316: Add loading panel to maps (with spinner gif)BCCVL-311: Use the "taxon_name" instead of "raw_taxon_name" when downloading ALA occurrences in the data mover
    Add unit test

--- data_mover/tests/services/test_ala_service.py
+++ data_mover/tests/services/test_ala_service.py
@@ -104,3 +104,56 @@ class TestALAService(unittest.TestCase):
             self.assertEqual(1, header.count(SPECIES))
             self.assertEqual(1, header.count(LONGITUDE))
             self.assertEqual(1, header.count(LATITUDE))
+
+    def test_get_rainbow_lorikeet_occurrence(self):
+        # This dataset is known to have different taxon_names appearing in the csv file.
+        # It takes the occurrences of the subspecies as well.
+        lsid = 'urn:lsid:biodiversity.org.au:afd.taxon:f3664294-203f-4d44-970c-06dd2a6c45cd'
+
+        dataset_factory = DatasetFactory()
+
+        ala_service = ALAService(dataset_factory)
+        ala_service._occurrence_url = "http://biocache.ala.org.au/ws/webportal/occurrences.gz?q=lsid:${lsid}&fq=geospatial_kosher:true&fl=taxon_name,longitude,latitude&pageSize=999999999"
+        dataset_factory._occurrence_url = ala_service._occurrence_url
+        ala_service._metadata_url = "http://bie.ala.org.au/species/${lsid}.json"
+
+        dest_dir = '/tmp/'
+        local_dest_dir = tempfile.mkdtemp()
+
+        out_files = ala_service.download_occurrence_by_lsid(lsid, dest_dir, 1234, 1, local_dest_dir)
+        out_files = listdir_fullpath(local_dest_dir)
+        self.assertEqual(3, len(out_files))
+
+        # The occurrence file exists
+        occurrence_file = os.path.join(local_dest_dir, 'move_job_1234_1_ala_occurrence.csv')
+        self.assertTrue(os.path.isfile(occurrence_file))
+
+        # The metadata file exists
+        metadata_file = os.path.join(local_dest_dir, 'move_job_1234_1_ala_metadata.json')
+        self.assertTrue(os.path.isfile(metadata_file))
+
+        # The dataset file exists
+        dataset_file = os.path.join(local_dest_dir, 'move_job_1234_1_ala_dataset.json')
+        self.assertTrue(os.path.isfile(dataset_file))
+
+        # The occurrence file has been normalized
+        with io.open(occurrence_file, mode='r+') as f:
+            lines = f.readlines()
+            self.assertTrue(len(lines) > 1)
+            header = lines[0]
+            self.assertEqual(1, header.count(SPECIES))
+            self.assertEqual(1, header.count(LONGITUDE))
+            self.assertEqual(1, header.count(LATITUDE))
+
+        # The species column is consistent
+        with io.open(occurrence_file, mode='r+') as f:
+            lines = f.readlines()
+            expected_species_name = None
+            for line in lines:
+                values = line.split(',')
+                actual_species_name = values[0]
+                if actual_species_name != 'species':
+                    if expected_species_name is None:
+                        expected_species_name = actual_species_name
+                    else:
+                        self.assertEqual(expected_species_name, actual_species_name)
