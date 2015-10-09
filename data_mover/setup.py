ea6b6d30b6fdacdc18a8f9ea597a10a66c9b9ea4
Gerhard Weis <g.weis@griffith.edu.au>
2015-10-09 12:35:45 +1000

    back to development

--- data_mover/setup.py
+++ data_mover/setup.py
@@ -39,7 +39,7 @@ extras_require = {
 
 
 setup(name='data_mover',
-      version='1.7.1',
+      version='1.7.2-dev',
       description='data_mover',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
